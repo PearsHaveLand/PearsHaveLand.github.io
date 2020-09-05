d7ea993b66998ce86a3e3ef4be32623f9508fd53
PearsHaveLand <pearce.haviland@protonmail.com>
2020-09-04 22:27:07 -0400

    Got session-based theme persistence working

--- darkmode.js
+++ darkmode.js
@@ -6,29 +6,38 @@ var nord4 = "#d8dee9";
 var nord5 = "#e5e9f0";
 var nord6 = "#eceff4";
 var darkLinkColor = "#abb9cf";
-var darkMode = true;
+var colorTheme = "dark";
 var selectionBorderFocusedColor = nord3;
 var selectionBorderBlurColor = nord1;
 
 function toggleDarkMode()
 {
-    // Set to lightmode
-    if(darkMode == true)
+    if (sessionStorage.getItem("colorTheme") == null)
     {
-        document.documentElement.style.setProperty('--jumbotron-background', nord5);
-        document.documentElement.style.setProperty('--content-background', nord6);
-        document.documentElement.style.setProperty('--body-text-color', nord0);
-        document.documentElement.style.setProperty('--link-hover-color', nord1);
-        document.documentElement.style.setProperty('--link-color', nord3);
-        document.documentElement.style.setProperty('--nav-hover-background', nord4);
-        document.documentElement.style.setProperty('--dropdown-select-color', darkLinkColor);
-        document.documentElement.style.setProperty('--selection-border-focused-color', darkLinkColor);
-        document.documentElement.style.setProperty('--selection-border-blur-color', nord4);
-        selectionBorderFocusedColor = darkLinkColor;
-        selectionBorderBlurColor = nord4;
+        colorTheme = "dark";
+    }
+    else
+    {
+        colorTheme = sessionStorage.getItem("colorTheme");
+    }
+
+    // Set to lightmode
+    if(colorTheme == "dark")
+    {
+        setTheme("light");
     }
     // Set to darkmode
     else
+    {
+        setTheme("dark");
+    }
+    sessionStorage.setItem("colorTheme", colorTheme);
+    console.log(colorTheme);
+}
+
+function setTheme(theme)
+{
+    if (theme == "dark")
     {
         document.documentElement.style.setProperty('--jumbotron-background', nord1);
         document.documentElement.style.setProperty('--content-background', nord0);
@@ -41,6 +50,30 @@ function toggleDarkMode()
         document.documentElement.style.setProperty('--selection-border-blur-color', nord1);
         selectionBorderFocusedColor = nord3;
         selectionBorderBlurColor = nord1;
+        colorTheme = "dark";
     }
-    darkMode = !darkMode;
+    else
+    {
+        document.documentElement.style.setProperty('--jumbotron-background', nord5);
+        document.documentElement.style.setProperty('--content-background', nord6);
+        document.documentElement.style.setProperty('--body-text-color', nord0);
+        document.documentElement.style.setProperty('--link-hover-color', nord1);
+        document.documentElement.style.setProperty('--link-color', nord3);
+        document.documentElement.style.setProperty('--nav-hover-background', nord4);
+        document.documentElement.style.setProperty('--dropdown-select-color', darkLinkColor);
+        document.documentElement.style.setProperty('--selection-border-focused-color', darkLinkColor);
+        document.documentElement.style.setProperty('--selection-border-blur-color', nord4);
+        selectionBorderFocusedColor = darkLinkColor;
+        selectionBorderBlurColor = nord4;
+        colorTheme = "light";
+    }
 }
+
+window.addEventListener("load", function(event){
+    console.log(sessionStorage.getItem("colorTheme"));
+    if (sessionStorage.getItem("colorTheme") == "light")
+    {
+        console.log("toggling");
+        setTheme("light");
+    }
+})
